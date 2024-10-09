af8162aae9ad9d92d4005fbd4f25edf237767470
Porter T Dansie <dansie.porter@gmail.com>
2024-10-09 11:18:11 -0600

    Finished Lab

--- components/tone/tone.c
+++ components/tone/tone.c
@@ -7,6 +7,9 @@
 #define DAC_RANGE 256
 #define WAVE_AMPLITUDE DAC_RANGE / 2
 #define BIAS DAC_RANGE / 2
+#define HALF_PERIOD 2
+#define QUARTER_PERIOD 4
+#define NATURAL_PERIOD 2 * M_PI
 
 uint8_t * g_buffer;
 uint32_t g_sample_hz;
@@ -44,22 +47,29 @@ int32_t tone_deinit(void) {
 void tone_start(tone_t tone, uint32_t freq) {
     uint32_t period_samples = g_sample_hz / freq;
 
-    for (int x = 0; x < period_samples; x++) {
-        switch(tone) {
+    //printf("Tone Start\nSample Rate:%ld Frequency:%ld\n", g_sample_hz, freq);
+    for (int x = 0; x < period_samples; x++) { // Generate Waveform
+        switch(tone) { // Switch logic based on chosen waveform
             case SINE_T:
-                g_buffer[x] = WAVE_AMPLITUDE * sinf(2 * M_PI * (float) freq * (float) x / (float) period_samples) + BIAS;
-                printf("x:%d buffer;%d\n", x, g_buffer[x]);
+                g_buffer[x] = WAVE_AMPLITUDE * sinf(NATURAL_PERIOD * (float) freq * (float) x / (float) g_sample_hz) + BIAS;
                 break;
             case SQUARE_T:
+                if (x < period_samples / HALF_PERIOD) g_buffer[x] = DAC_RANGE - 1;
+                else g_buffer[x] = 0;
                 break;
             case TRIANGLE_T:
+                if (x < period_samples / QUARTER_PERIOD) g_buffer[x] = BIAS + (DAC_RANGE / (period_samples / HALF_PERIOD)) * x ; // First Quarter
+                else if (x < period_samples / QUARTER_PERIOD +  period_samples / HALF_PERIOD) g_buffer[x] = BIAS - (DAC_RANGE / (period_samples / HALF_PERIOD)) * (x - period_samples / HALF_PERIOD); // Second and Third Quarters - Shifted
+                else g_buffer[x] = BIAS + (DAC_RANGE / (period_samples / HALF_PERIOD)) * (x - period_samples); // Last Quarter - Shifter
                 break;
             case SAW_T:
+                if (x < period_samples / HALF_PERIOD) g_buffer[x] = BIAS + (DAC_RANGE / period_samples) * x; // First Half
+                else g_buffer[x] = BIAS + (DAC_RANGE / period_samples) * (x - period_samples); // Second Half - Shifted
                 break;
             case LAST_T:
                 break;
         }
+        //printf("x:%2d buffer:%d\n", x, g_buffer[x]);
     }
-
     sound_cyclic(g_buffer, period_samples);
 }
