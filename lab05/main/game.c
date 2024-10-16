a8b9e6befcb54147d709a569666d0f9db021aae2
Porter T Dansie <dansie.porter@gmail.com>
2024-10-16 11:27:04 -0600

    M1 complete

--- lab05/main/game.c
+++ lab05/main/game.c
@@ -1,4 +1,7 @@
 #include "game.h"
+
+#include <config.h>
+
 #include "pin.h"
 #include "graphics.h"
 
@@ -7,6 +10,15 @@
 #include <hw_gc.h>
 #include <lcd.h>
 #include <nav.h>
+#include <stdio.h>
+
+#define MESSAGE_COLOR WHITE
+#define MESSAGE_BG_COLOR GRAY
+#define X_COLOR RED
+#define O_COLOR BLUE
+#define GRID_COLOR WHITE
+#define BACKGROUND_COLOR BLACK
+
 
 typedef enum {
     INIT_ST,
@@ -21,18 +33,50 @@ mark_t current_turn_g;
 mark_t game_winner_g;
 
 game_state_t current_state_g;
-void mark() {
+
+void start_game(void) {
+    lcd_fillRect(0, 0, LCD_W, LCD_H, BACKGROUND_COLOR);
+    board_clear();
+    graphics_drawGrid(GRID_COLOR);
+    graphics_drawMessage("", MESSAGE_COLOR, MESSAGE_BG_COLOR);
+
+    game_winner_g = no_m;
+    current_turn_g = X_m;
+}
+
+void mark(void) {
     int8_t curr_r, curr_c;
     nav_get_loc(&curr_r, &curr_c);
     if (!board_set(curr_r,curr_c, current_turn_g)) {
-        graphics_drawMessage("Space already Occupied", WHITE, GRAY);
+        graphics_drawMessage("Space already Occupied", MESSAGE_COLOR, MESSAGE_BG_COLOR);
     } else {
         if (current_turn_g == X_m) {
-            graphics_drawX(curr_r, curr_c, RED);
+            graphics_drawX(curr_r, curr_c, X_COLOR);
             current_turn_g = O_m;
         } else {
-            graphics_drawO(current_turn_g, curr_r, BLUE);
+            graphics_drawO(curr_r, curr_c, O_COLOR);
             current_turn_g = X_m;
+        }
+    }
+}
+
+void check_mark_transition(void) {
+    if (game_winner_g) {
+        current_state_g = WAIT_RESTART_ST;
+        const char winner_char = game_winner_g == X_m ? 'X' : 'O';
+        char winner_message[33];
+        sprintf(winner_message, "%c Wins! Press Start to restart.", winner_char);
+        graphics_drawMessage(winner_message, MESSAGE_COLOR, MESSAGE_BG_COLOR);
+    } else {
+        if (board_mark_count() == CONFIG_BOARD_R * CONFIG_BOARD_C) {
+            current_state_g = WAIT_RESTART_ST;
+        graphics_drawMessage("Draw! Press Start to restart.", MESSAGE_COLOR, MESSAGE_BG_COLOR);
+        } else {
+            current_state_g = WAIT_MARK_ST;
+            const char current_turn = current_turn_g == X_m ? 'X' : 'O';
+            char turn_message[40];
+            sprintf(turn_message, "%c's turn. Press A to place your mark.", current_turn);
+            graphics_drawMessage(turn_message, MESSAGE_COLOR, MESSAGE_BG_COLOR);
         }
     }
 }
@@ -42,16 +86,23 @@ void game_init(void) {
     current_state_g = INIT_ST;
     current_turn_g = X_m;
     game_winner_g = no_m;
+    board_clear();
 }
 
 // Update the game logic.
 void game_tick(void) {
-    switch (current_state_g) { //Transition Logic
+    //printf("Current state: %d\n", current_state_g);
+
+    switch (current_state_g) { //Transition Logic and Actions
         case INIT_ST:
             current_state_g = NEW_GAME_ST;
             break;
         case NEW_GAME_ST:
             current_state_g = WAIT_MARK_ST;
+            const char current_turn = current_turn_g == X_m ? 'X' : 'O';
+            char turn_message[40];
+            sprintf(turn_message, "%c's turn. Press A to place your mark.", current_turn);
+            graphics_drawMessage(turn_message, MESSAGE_COLOR, MESSAGE_BG_COLOR);
             break;
         case WAIT_MARK_ST:
             if (!pin_get_level(HW_BTN_A)) {
@@ -64,11 +115,7 @@ void game_tick(void) {
             current_state_g = CHECK_MARK_ST;
             break;
         case CHECK_MARK_ST:
-            if (!game_winner_g) {
-                current_state_g = WAIT_MARK_ST;
-            } else {
-                current_state_g = WAIT_RESTART_ST;
-            }
+            check_mark_transition();
             break;
         case WAIT_RESTART_ST:
             if (!pin_get_level(HW_BTN_START)) {
@@ -79,13 +126,11 @@ void game_tick(void) {
             break;
     }
 
-    switch (current_state_g) {
+    switch (current_state_g) { //State Actions
         case INIT_ST:
             break;
         case NEW_GAME_ST:
-            graphics_drawGrid(WHITE);
-            game_winner_g = no_m;
-            current_turn_g = X_m;
+            start_game();
             break;
         case WAIT_MARK_ST:
             break;
